73c79f35314533b34f87f49e2b5d6f4c4607a5ad
Joseph Tascona <joseph.tascona@gmail.com>
2022-09-28 14:30:25 -0400

    Added all steps for take it further

--- js/app.js
+++ js/app.js
@@ -25,6 +25,9 @@ let button5 = document.querySelector('#button5');
 // array for words
 let arrToSpeak = ["","","","",""];
 
+// Take it further 4.
+let arrToText = "";
+
 /* Functions
 -------------------------------------------------- */
 function speakNow(string) {
@@ -41,29 +44,90 @@ speakButton.onclick = function() {
 	textToSpeak = arrToSpeak.join(" ");
 	console.log(textToSpeak);
 	speakNow(textToSpeak);
+	// Take it further 4.
+	textStoryFunc(textToSpeak);
 }
 
 button1.onclick = function() {
 	let rand = Math.floor(Math.random() * arr1.length);
 	arrToSpeak[0] = arr1[rand];
+	// Take it further 4.
+	arrToText = arrToSpeak.join(" ");
+	textStoryFunc(arrToText);
+	
 }
 
 button2.onclick = function () {
 	let rand = Math.floor(Math.random() * arr2.length);
 	arrToSpeak[1] = arr2[rand];
+	// Take it further 4.
+	arrToText = arrToSpeak.join(" ");
+	textStoryFunc(arrToText);
 }
 
 button3.onclick = function () {
 	let rand = Math.floor(Math.random() * arr3.length);
 	arrToSpeak[2] = arr3[rand];
+	// Take it further 4.
+	arrToText = arrToSpeak.join(" ");
+	textStoryFunc(arrToText);
 }
 
 button4.onclick = function () {
 	let rand = Math.floor(Math.random() * arr4.length);
 	arrToSpeak[3] = arr4[rand];
+	// Take it further 4.
+	arrToText = arrToSpeak.join(" ");
+	textStoryFunc(arrToText);
 }
 
 button5.onclick = function () {
 	let rand = Math.floor(Math.random() * arr5.length);
 	arrToSpeak[4] = arr5[rand];
+	// Take it further 4.
+	arrToText = arrToSpeak.join(" ");
+	textStoryFunc(arrToText);
+}
+
+// TAKE IT FURTHER
+// 1.
+let randomStoryButton = document.querySelector('#randomStoryButton');
+
+let arrToSpeak2 = ["","","","",""];
+
+let textToSpeak2 = "";
+
+let totalArr = [["The turkey", "Mom", "Dad", "The dog", "My teacher", "The elephant", "The cat"],
+				["sat on", "ate", "danced with", "saw", "doesn't like", "kissed"],
+				["a funny", "a scary", "a goofy", "a slimy", "a barking", "a fat"],
+				["goat", "monkey", "fish", "cow", "frog", "bug", "worm"],
+				["on the moon", "on the chair", "in my spaghetty", "in my soup", "on the grass", "in my shoes"]];
+
+randomStoryButton.onclick = function() {
+	for (let i = 0; i < arrToSpeak2.length; i++) {
+		let rand = Math.floor(Math.random() * totalArr[i].length);
+		arrToSpeak2[i] = totalArr[i][rand];
+	}
+	textToSpeak2 = arrToSpeak2.join(" ");
+	console.log(textToSpeak2);
+	speakNow(textToSpeak2);
+	// Take it further 4.
+	textStoryFunc(textToSpeak2);
+}
+
+// 2.
+let resetButton = document.querySelector('#reset');
+
+resetButton.onclick = function() {
+	arrToSpeak = ["","","","",""];
+	arrToSpeak2 = ["","","","",""];
+	textStoryFunc("");
+	textStory.style.display = "none";
+}
+
+// 4.
+let textStory = document.querySelector('#textStory');
+function textStoryFunc(string) {
+	textStory.textContent = string;
+	textStory.style.display = "block";
 }
